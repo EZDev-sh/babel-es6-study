93a16df1398dcbbef2500e154e57e2e3c564094c
EZDev <ezdev@EZDevui-MacBookPro.local>
2019-02-10 20:38:21 +0900

    includes

---
+++ src/includes_.js
@@ -0,0 +1,10 @@
+// 배열에 사용되는 문법
+let years = [2001, 2002, 2003, 2004]
+const fruits = ['apple', 'banana', 'potato']
+
+// find value (before)
+console.log(years.indexOf(2004))
+console.log(fruits.indexOf('apple') != -1)
+
+// find value (now)
+console.log(fruits.includes('grape'))
