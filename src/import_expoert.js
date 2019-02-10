39a6a95ce67d48fffd1558143e0341f358c2ed18
EZDev <ezdev@EZDevui-MacBookPro.local>
2019-02-10 21:05:45 +0900

    variable import and export

---
+++ src/import_expoert.js
@@ -0,0 +1,8 @@
+// export default X ( {} : 여러개의 변수를 가지고 올때 사용)
+// import { fruits, test } from './includes_'
+// console.log(fruits)
+// console.log(test)
+
+// export default O
+import fruits from './includes_'
+console.log(fruits)
