4300271a712f8a4e7f7925a71a8a14fbe86627a7
EZDev <ezdev@EZDevui-MacBookPro.local>
2019-02-10 21:21:23 +0900

    traling comma

---
+++ src/traling_comma.js
@@ -0,0 +1,14 @@
+const myObj = {
+    first: 'test1',
+    second: 'test2',
+}
+console.log(myObj)
+
+const myArr = [
+    1,
+    2,
+    3,
+]
+console.log(myArr)
+
+// traling comma: 객체안의 값을 정의할때 추가의 의미로 사용되는 ','가 마지막에 추가되어도 된다.
